80f68a8db3dc0790bb40b4ebfe2f18698457150c
cuongpm0217 <cuongpm0217@gmail.com>
2024-05-14 12:54:13 +0700

    fix enum Role

--- ver1/src/main/java/com/hongha/ver1/reporitories/RoleReporitory.java
+++ ver1/src/main/java/com/hongha/ver1/reporitories/RoleReporitory.java
@@ -8,18 +8,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD
 import com.hongha.ver1.entities.Role;
 import com.hongha.ver1.entities.enums.ERole;
-=======
-import com.hongha.ver1.entities.ERole;
-import com.hongha.ver1.entities.Role;
->>>>>>> a833e23cca83a037a87c92ee39e57e07f3c32774
-
 
 @Repository
 public interface RoleReporitory extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(ERole name);
-    @Query(value = "select r from _role r where r.gen_id= :uuid", nativeQuery = true)
+	Optional<Role> findByName(ERole name);
+
+	@Query(value = "select r from _role r where r.gen_id= :uuid", nativeQuery = true)
 	Role findByUUID(@Param("uuid") UUID genID);
 }
--- ver1/src/main/java/com/hongha/ver1/services/IRoleService.java
+++ ver1/src/main/java/com/hongha/ver1/services/IRoleService.java
@@ -3,23 +3,26 @@ package com.hongha.ver1.services;
 import java.util.List;
 import java.util.UUID;
 
-<<<<<<< HEAD
 import com.hongha.ver1.entities.Role;
 import com.hongha.ver1.entities.enums.ERole;
-=======
-import com.hongha.ver1.entities.ERole;
-import com.hongha.ver1.entities.Role;
->>>>>>> a833e23cca83a037a87c92ee39e57e07f3c32774
 
 public interface IRoleService {
 	Role save(Role RoleRequest);
+
 	Role findById(long id);
+
 	Role findByUUID(UUID genId);
+
 	Role findByName(ERole name);
+
 	List<Role> getAll();
-	Role update(long id,Role RoleRequest);
+
+	Role update(long id, Role RoleRequest);
+
 	void delete(long id);
-	Role updateByUUID(UUID genID,Role RoleRequest);
+
+	Role updateByUUID(UUID genID, Role RoleRequest);
+
 	void deleteByUUID(UUID genID);
-	
+
 }
