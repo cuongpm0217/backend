75b1dbe0078c94ec0c26e0a4d0e0731c12fe0453
cuongpm0217 <cuongpm0217@gmail.com>
2024-05-14 12:47:14 +0700

    add accountdto

---
+++ ver1/src/main/java/com/hongha/ver1/dtos/AcountingDTO.java
@@ -0,0 +1,16 @@
+package com.hongha.ver1.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AcountingDTO extends BaseDTO{
+	private String code;// 131 311..	
+	private String name;// phải thu của khách....	
+	private int level;// 1-131 2-1311 2-1312
+}
