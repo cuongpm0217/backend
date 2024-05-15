9222de5567ae75ad04667f1dfeeb8b0019910dc6
cuongpm0217 <cuongpm0217@gmail.com>
2024-05-15 17:18:01 +0700

    Adding salary entity

---
+++ ver1/src/main/java/com/hongha/ver1/entities/Salary.java
@@ -0,0 +1,29 @@
+package com.hongha.ver1.entities;
+
+import java.math.BigInteger;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "_salary")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Salary {
+	@Column
+	private String name;
+	@Column
+	private long salaryTypeId;
+	@Column
+	private long employeeId;
+	@Column
+	private BigInteger wage;
+	
+}
---
+++ ver1/src/main/java/com/hongha/ver1/entities/SalaryType.java
@@ -0,0 +1,26 @@
+package com.hongha.ver1.entities;
+
+
+import com.hongha.ver1.entities.enums.ESalaryType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="_salary_type")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SalaryType {
+	@Enumerated(EnumType.STRING)
+	@Column
+	private ESalaryType name;
+}
