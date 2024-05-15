03353fc72437aaf64ab8a3aee801cd07bf9e46c7
cuongpm0217 <cuongpm0217@gmail.com>
2024-05-15 17:52:31 +0700

    Adding Salary DTO

---
+++ ver1/src/main/java/com/hongha/ver1/dtos/SalaryDTO.java
@@ -0,0 +1,19 @@
+package com.hongha.ver1.dtos;
+
+import java.math.BigInteger;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SalaryDTO extends BaseDTO {
+	private String name;
+	private long salaryTypeId;
+	private long employeeId;
+	private BigInteger wage;
+
+}
