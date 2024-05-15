5973e809d0faa67e1e075322ed7f5e355eb51447
cuongpm0217 <cuongpm0217@gmail.com>
2024-05-15 17:17:46 +0700

    Create SurrogateDTO.java

---
+++ ver1/src/main/java/com/hongha/ver1/dtos/SurrogateDTO.java
@@ -0,0 +1,16 @@
+package com.hongha.ver1.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SurrogateDTO extends BaseDTO {
+	private String name;
+	private String phone;
+	private String address;
+}
