a79ccba68abb5f5486d6b9f0cfe55e12e084772b
cuongpm0217 <cuongpm0217@gmail.com>
2024-05-22 10:08:44 +0700

    Add generate code > invoice

---
+++ ver1/src/main/java/com/hongha/ver1/utils/GenerateCode.java
@@ -0,0 +1,29 @@
+package com.hongha.ver1.utils;
+
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class GenerateCode {
+
+	public String GenInvoiceCode(int countInYear, String invoiceType) {
+		String code = "";
+		SimpleDateFormat df = new SimpleDateFormat("yyyy");
+		String date = df.format(new Date());
+		DecimalFormat nf = new DecimalFormat("000000");
+		if (countInYear == 0) {
+			countInYear = 1;
+		}
+		String count = nf.format(countInYear);
+		switch (invoiceType) {
+		case "Purchase": {
+			code = "PM";
+		}
+		case "RepairBill": {
+			code = "DV";
+		}
+		}
+		return code += date + count;
+	}
+}
