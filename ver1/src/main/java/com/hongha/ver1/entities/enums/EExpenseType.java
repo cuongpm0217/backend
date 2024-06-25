62834004b9e382e72afa13d948fc46abdeccddbc
cuongpm0217 <cuongpm0217@gmail.com>
2024-06-26 01:11:50 +0700

    change name Enum and lable

--- ver1/src/main/java/com/hongha/ver1/entities/enums/EExpenseType.java
+++ ver1/src/main/java/com/hongha/ver1/entities/enums/EExpenseType.java
@@ -1,9 +1,9 @@
 package com.hongha.ver1.entities.enums;
 
 public enum EExpenseType {
-	Expense_RENTAL("Tiền thuê nhà"),
-	Expense_ELECTRIC("Tiền điện"),
-	Expense_WATER("Tiền nước"),
+	Expense_RENTAL("Mặt bằng"),
+	Expense_ELECTRIC("Điện"),
+	Expense_WATER("Nước"),
 	Expense_STATIONERY("Văn phòng phẩm"),
 	Expense_MONEY4FOOD("Tiền ăn"),
 	Expense_OTHERS("Khác");
--- ver1/src/main/java/com/hongha/ver1/entities/enums/EProductType.java
+++ ver1/src/main/java/com/hongha/ver1/entities/enums/EProductType.java
@@ -1,9 +1,9 @@
 package com.hongha.ver1.entities.enums;
 
 public enum EProductType {
-	EProductType_Product("SẢN PHẨM"),
-	EProductType_Tool("CÔNG CỤ"),
-	EProductType_Job("CÔNG VIỆC"),
+	EProductType_PRODUCT("SẢN PHẨM"),
+	EProductType_TOOL("CÔNG CỤ"),
+	EProductType_TASK("CÔNG VIỆC"),
 	EProductType_MATERIAL("VẬT LIỆU");
 	public final String label;
 	private EProductType(String label) {
