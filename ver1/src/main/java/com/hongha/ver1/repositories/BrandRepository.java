923cc1bf73d19cf3f4a9b29bbe5e4c1d763321d2
cuongpm0217 <cuongpm0217@gmail.com>
2024-06-26 01:15:29 +0700

    Change Product Brand > Brand to share

---
+++ ver1/src/main/java/com/hongha/ver1/repositories/BrandRepository.java
@@ -0,0 +1,13 @@
+package com.hongha.ver1.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hongha.ver1.entities.Brand;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+	Brand findByGenId(UUID genId);
+}
