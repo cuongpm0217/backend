53491a87da161bcf9ffa290c3ab6481f6f631799
cuongpm0217 <cuongpm0217@gmail.com>
2024-06-26 01:11:17 +0700

    Enable schedule

--- ver1/src/main/java/com/hongha/ver1/HongHaAutoApplication.java
+++ ver1/src/main/java/com/hongha/ver1/HongHaAutoApplication.java
@@ -4,9 +4,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class HongHaAutoApplication {
     @Bean
     ModelMapper modelMapper() {
@@ -14,9 +16,7 @@ public class HongHaAutoApplication {
 	}
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(HongHaAutoApplication.class, args);
-//		new LoadDefault().loadRole();
 	}
 
 }
