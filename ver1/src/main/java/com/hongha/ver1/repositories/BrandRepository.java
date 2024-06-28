c21df874d17d5544fc0689d61b11690b671ac306
cuongpm0217 <cuongpm0217@gmail.com>
2024-06-28 09:14:38 +0700

    adding find by return page

--- ver1/src/main/java/com/hongha/ver1/repositories/BrandRepository.java
+++ ver1/src/main/java/com/hongha/ver1/repositories/BrandRepository.java
@@ -2,6 +2,8 @@ package com.hongha.ver1.repositories;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,5 @@ import com.hongha.ver1.entities.Brand;
 @Repository
 public interface BrandRepository extends JpaRepository<Brand, Long> {
 	Brand findByGenId(UUID genId);
+	Page<Brand> findByNameLike(String name,Pageable pageable);
 }
