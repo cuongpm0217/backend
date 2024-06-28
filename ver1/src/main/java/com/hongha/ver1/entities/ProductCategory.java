e79b871194a246fbc667a33f5f7a10cac35fd9a4
cuongpm0217 <cuongpm0217@gmail.com>
2024-06-28 09:10:57 +0700

    Add Product Category model, dto, listener

---
+++ ver1/src/main/java/com/hongha/ver1/entities/ProductCategory.java
@@ -0,0 +1,25 @@
+package com.hongha.ver1.entities;
+
+import com.hongha.ver1.entities.listeners.ProducCategorytListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@EntityListeners(ProducCategorytListener.class)
+@Table(name = "_product_category")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductCategory extends BaseEntityAudit{
+	private static final long serialVersionUID = 1L;
+	@Column()
+	private String name;
+}
