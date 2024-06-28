b8a4d3e1e35b06604e9330244850b16016bf1800
cuongpm0217 <cuongpm0217@gmail.com>
2024-06-28 09:09:16 +0700

    Adding find by invoice id method

--- ver1/src/main/java/com/hongha/ver1/repositories/AccountInvoiceRepository.java
+++ ver1/src/main/java/com/hongha/ver1/repositories/AccountInvoiceRepository.java
@@ -1,7 +1,10 @@
 package com.hongha.ver1.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +13,6 @@ import com.hongha.ver1.entities.AccountInvoice;
 @Repository
 public interface AccountInvoiceRepository extends JpaRepository<AccountInvoice, Long> {
 	AccountInvoice findByGenId(UUID genId);
+	Page<AccountInvoice> findByInvoiceId(long invoiceId,Pageable pageable);
+	List<AccountInvoice> findByInvoiceId(long invoiceId);
 }
