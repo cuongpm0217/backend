b7fd8072e3506d9103480764a1f07556f1d8b2af
cuongpm0217 <cuongpm0217@gmail.com>
2024-07-07 08:39:01 +0700

    Using UUID with User in Auth

--- ver1/src/main/java/com/hongha/ver1/securties/jwt/payload/response/JwtResponse.java
+++ ver1/src/main/java/com/hongha/ver1/securties/jwt/payload/response/JwtResponse.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.List;
+import java.util.UUID;
 
 @AllArgsConstructor
 @Getter
@@ -11,13 +12,13 @@ import java.util.List;
 public class JwtResponse {
     private String token;
     private String type="honghaauto";
-    private Long id;
+    private UUID genId;
     private String userName;
     private String email;
     private List<String> roles;
-    public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles) {
+    public JwtResponse(String accessToken, UUID genId, String username, String email, List<String> roles) {
         this.token = accessToken;
-        this.id = id;
+        this.genId = genId;
         this.userName = username;
         this.email = email;
         this.roles = roles;
