855c2f1300a7b72de2fe55b0db7776fa9e7e15c8
cuongpm0217 <cuongpm0217@gmail.com>
2024-07-07 16:06:29 +0700

    remove Query, Param in repository

--- ver1/src/main/java/com/hongha/ver1/repositories/PurchaseRepository.java
+++ ver1/src/main/java/com/hongha/ver1/repositories/PurchaseRepository.java
@@ -6,7 +6,6 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.hongha.ver1.entities.Purchase;
@@ -20,7 +19,5 @@ public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
 	Page<Purchase> findByPartnerIdAndCreatedAtBetween(long partnerId, Date fromDate, Date toDate, Pageable paging);
 
 	Page<Purchase> findByCreatedAtBetween(Date fromDate, Date toDate, Pageable paging);
-	
-//	@Query("select count(p.id) from _purchase p where year(p.created_at)=year(now())")
-//	double total();
+
 }
