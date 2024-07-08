79e76e44cf25a3d46cc39509b73838ebbffa4521
cuongpm0217 <cuongpm0217@gmail.com>
2024-07-09 01:23:37 +0700

    add find by name | address

--- ver1/src/main/java/com/hongha/ver1/repositories/BranchRepository.java
+++ ver1/src/main/java/com/hongha/ver1/repositories/BranchRepository.java
@@ -2,6 +2,8 @@ package com.hongha.ver1.repositories;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ import com.hongha.ver1.entities.Branch;
 @Repository
 public interface BranchRepository extends JpaRepository<Branch, Long> {
 	Branch findByGenId(UUID genId);
+	
+	Page<Branch> findByNameOrAddressLike(String name,String address, Pageable paging);
 }
