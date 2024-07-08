28a5504b9993d545cb5b36fa1a11a9519d38ba82
cuongpm0217 <cuongpm0217@gmail.com>
2024-07-09 01:23:22 +0700

    add find by code contain

--- ver1/src/main/java/com/hongha/ver1/repositories/CurrencyRepository.java
+++ ver1/src/main/java/com/hongha/ver1/repositories/CurrencyRepository.java
@@ -2,13 +2,18 @@ package com.hongha.ver1.repositories;
 
 
 import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hongha.ver1.entities.Currency;
 
+
 @Repository
 public interface CurrencyRepository extends JpaRepository<Currency, Long> {
 	Currency findByGenId(UUID genId);
 	Currency findByCode(String code);
+	Page<Currency> findByCodeContaining(String code,Pageable pageable);
 }
