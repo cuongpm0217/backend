0301362ae8dae4eb457e7f3e104e3ad38c770515
cuongpm0217 <cuongpm0217@gmail.com>
2024-07-22 08:06:02 +0700

    add find by uuid

--- ver1/src/main/java/com/hongha/ver1/repositories/UserRepository.java
+++ ver1/src/main/java/com/hongha/ver1/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.hongha.ver1.repositories;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
+    User findByGenId(UUID genId);
 
 }
