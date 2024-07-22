d892c3683995d0aeadf37f8d1e6c47219143ec71
cuongpm0217 <cuongpm0217@gmail.com>
2024-07-22 07:47:35 +0700

    Add search something method

--- ver1/src/main/java/com/hongha/ver1/repositories/DepartmentRepository.java
+++ ver1/src/main/java/com/hongha/ver1/repositories/DepartmentRepository.java
@@ -3,8 +3,10 @@ package com.hongha.ver1.repositories;
 import java.util.UUID;
 
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.hongha.ver1.entities.Department;
@@ -12,5 +14,23 @@ import com.hongha.ver1.entities.Department;
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long>{
 	Department findByGenId(UUID genId);
-	Slice<Department> findByBranchIdAndVnameLike(long branchId,String vName,Pageable paging);
+	
+	@Query(value= "SELECT _d.* FROM `_department` as _d "
+			+ "LEFT JOIN `_branch` _b ON _d.branch_id = _b.id "
+			+ "WHERE "			
+			+ "(SELECT CONVERT(_d.updated_at,DATE)) LIKE CONCAT('%', CONCAT(:searchText, '%')) "
+			+ "OR LOWER(_b.name) LIKE lower(concat('%', concat(:searchText, '%'))) "
+			+ "OR LOWER(_d.code) LIKE lower(concat('%', concat(:searchText, '%'))) "
+			+ "OR LOWER(_d.name) LIKE lower(concat('%', concat(:searchText, '%'))) "
+			+ "OR LOWER(_d.vname) LIKE lower(concat('%', concat(:searchText, '%'))) "
+			,countQuery = "SELECT count(_d.id) FROM `_department` as _d "
+					+ "LEFT JOIN `_branch` _b ON _d.branch_id = _b.id "
+					+ "WHERE "					
+					+ "(SELECT CONVERT(_d.updated_at,DATE)) LIKE CONCAT('%', CONCAT(:searchText, '%')) "
+					+ "OR LOWER(_b.name) LIKE lower(concat('%', concat(:searchText, '%'))) "
+					+ "OR LOWER(_d.code) LIKE lower(concat('%', concat(:searchText, '%'))) "
+					+ "OR LOWER(_d.name) LIKE lower(concat('%', concat(:searchText, '%'))) "
+					+ "OR LOWER(_d.vname) LIKE lower(concat('%', concat(:searchText, '%'))) "
+			,nativeQuery = true)
+	Page<Department> findBySearchText(@Param("searchText") String searchText, Pageable pageable);
 }
