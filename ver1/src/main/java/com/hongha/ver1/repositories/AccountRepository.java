fa58aa977f901f45ae5773b49347168abb9baf0e
cuongpm0217 <cuongpm0217@gmail.com>
2024-08-13 11:12:37 +0700

    Update AccountRepository.java
    Add find by search

--- ver1/src/main/java/com/hongha/ver1/repositories/AccountRepository.java
+++ ver1/src/main/java/com/hongha/ver1/repositories/AccountRepository.java
@@ -5,11 +5,23 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.hongha.ver1.entities.Account;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 	Account findByGenId(UUID genId);
 
-	Page<Account> findByCodeContaining(String code, Pageable paging);
+	@Query(value= "SELECT * FROM _account a "
+			+ "WHERE "
+			+ "(SELECT CONVERT(a.updated_at,DATE)) LIKE CONCAT('%', CONCAT(:searchText, '%')) "
+			+ "OR LOWER(a.code) LIKE lower(concat('%', concat(:searchText, '%'))) "
+			+ "OR LOWER(a.name) LIKE lower(concat('%', concat(:searchText, '%'))) ",
+			countQuery = "SELECT count(a.id) FROM _account a WHERE "
+					+ "LOWER(a.code) LIKE lower(concat('%', concat(:searchText, '%'))) "
+					+ "OR LOWER(a.name) LIKE lower(concat('%', concat(:searchText, '%'))) ",
+			nativeQuery = true)
+	Page<Account> findBySearchText(@Param("searchText") String searchText, Pageable pageable);
 }
