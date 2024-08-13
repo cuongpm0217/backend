570b605222c3158ffb33494b463d0b5f89bd97cc
cuongpm0217 <cuongpm0217@gmail.com>
2024-08-13 11:16:18 +0700

    Update LoginRequest.java
    Add Ip, device, reloadToken

--- ver1/src/main/java/com/hongha/ver1/securties/jwt/payload/request/LoginRequest.java
+++ ver1/src/main/java/com/hongha/ver1/securties/jwt/payload/request/LoginRequest.java
@@ -1,14 +1,15 @@
 package com.hongha.ver1.securties.jwt.payload.request;
 
 import lombok.Getter;
-import lombok.Setter;
 
-@Setter
 @Getter
 public class LoginRequest {
 
     private String username;
     private String password;
     private long branchId;
+    private String ipLogin;
+    private String device;
+    private String reloadToken;
 
 }
