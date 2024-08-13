6976019aa291f46254721cde5bdc176b6c31402b
cuongpm0217 <cuongpm0217@gmail.com>
2024-08-13 08:24:29 +0700

    Move class util > configuration

--- ver1/src/main/java/com/hongha/ver1/configuration/AuditorAwareConfig.java
+++ ver1/src/main/java/com/hongha/ver1/configuration/AuditorAwareConfig.java
@@ -1,13 +1,15 @@
-package com.hongha.ver1.utils;
+package com.hongha.ver1.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import com.hongha.ver1.utils.AuditorAwareImpl;
+
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
-public class JpaConfig {
+public class AuditorAwareConfig {
     @Bean
     AuditorAware<String> auditorAware() {
         return new AuditorAwareImpl();
