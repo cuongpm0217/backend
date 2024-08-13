892bc3d7614428bb5f74ff3ba2e0c50576ab1290
cuongpm0217 <cuongpm0217@gmail.com>
2024-08-13 11:13:07 +0700

    Update PositionRepository.java
    Add find by search

--- ver1/src/main/java/com/hongha/ver1/repositories/PositionRepository.java
+++ ver1/src/main/java/com/hongha/ver1/repositories/PositionRepository.java
@@ -5,12 +5,25 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import com.hongha.ver1.entities.Position;
 
 @Repository
 public interface PositionRepository extends JpaRepository<Position, Long> {
 	Position findByGenId(UUID genId);
 	Page<Position> findByVnameLike(String vname,Pageable paging);
+	@Query(value= "SELECT _p.* FROM `_position` as _p "			
+			+ "WHERE "			
+			+ "(SELECT CONVERT(_p.updated_at,DATE)) LIKE CONCAT('%', CONCAT(:searchText, '%')) "			
+			+ "OR LOWER(_p.name) LIKE lower(concat('%', concat(:searchText, '%'))) "
+			+ "OR LOWER(_p.vname) LIKE lower(concat('%', concat(:searchText, '%'))) "
+			,countQuery = "SELECT count(_p.id) FROM `_position` as _p "			
+					+ "WHERE "			
+					+ "(SELECT CONVERT(_p.updated_at,DATE)) LIKE CONCAT('%', CONCAT(:searchText, '%')) "			
+					+ "OR LOWER(_p.name) LIKE lower(concat('%', concat(:searchText, '%'))) "
+					+ "OR LOWER(_p.vname) LIKE lower(concat('%', concat(:searchText, '%'))) "
+			,nativeQuery = true)
+	Page<Position> findBySearchText(@Param("searchText") String searchText, Pageable pageable);
 }
