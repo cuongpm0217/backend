3dae1f0d0034a82c82be5a91a2112492ca78e9d6
cuongpm0217 <cuongpm0217@gmail.com>
2024-08-13 11:12:03 +0700

    Update HongHaAutoApplication.java

--- ver1/src/main/java/com/hongha/ver1/HongHaAutoApplication.java
+++ ver1/src/main/java/com/hongha/ver1/HongHaAutoApplication.java
@@ -1,19 +1,13 @@
 package com.hongha.ver1;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableScheduling
 public class HongHaAutoApplication {
-    @Bean
-    ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HongHaAutoApplication.class, args);
