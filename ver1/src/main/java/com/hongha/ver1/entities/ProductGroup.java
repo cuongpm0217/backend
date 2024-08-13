350108c9ae0519b981cd0d9e990c04b7df7d98b5
cuongpm0217 <cuongpm0217@gmail.com>
2024-08-13 11:08:57 +0700

    Create ProductGroup.java

---
+++ ver1/src/main/java/com/hongha/ver1/entities/ProductGroup.java
@@ -0,0 +1,29 @@
+package com.hongha.ver1.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+//@EntityListeners(ProductGroupListener.class)
+@Table(name = "_product_group")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductGroup extends BaseEntityAudit{
+	
+	private static final long serialVersionUID = 4525858241774645250L;
+	@Column
+	private String name;
+	@Column
+	private String code;
+
+}
