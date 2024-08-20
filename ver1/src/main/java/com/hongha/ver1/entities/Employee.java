a4bfc1ffc8ce19e37dfee329a1bdf380104e3e96
cuongpm0217 <cuongpm0217@gmail.com>
2024-08-20 15:30:54 +0700

    Change Date > LocalDate

--- ver1/src/main/java/com/hongha/ver1/entities/Employee.java
+++ ver1/src/main/java/com/hongha/ver1/entities/Employee.java
@@ -1,7 +1,6 @@
 package com.hongha.ver1.entities;
 
-import java.util.Date;
-
+import java.time.LocalDate;
 import com.hongha.ver1.entities.listeners.EmployeeListener;
 
 import jakarta.persistence.Column;
@@ -9,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,6 +18,7 @@ import lombok.Setter;
 @Table(name = "_employee")
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Employee extends BaseEntityAudit {
@@ -25,16 +26,16 @@ public class Employee extends BaseEntityAudit {
 	private static final long serialVersionUID = 1L;
 	@Column
 	private String name;
-	@Column(name="user_id")
+	@Column(name = "user_id")
 	private long userId;
-	@Column(name="position_id")
+	@Column(name = "position_id")
 	private long positionId;
-	@Column(name="department_id")
+	@Column(name = "department_id")
 	private long departmentId;
-	@Column(name="branch_id")
+	@Column(name = "branch_id")
 	private long branchId;
 	@Column
-	private Date dob;
+	private LocalDate dob;
 	@Column
 	private boolean gender;
 	@Column(name = "national_id")
